f8b78b1b3084c9c15c2cbb92e14d4567f1d8f2ac
Emiaostein <cszwdy@gmail.com>
2020-07-05 10:27:46 +0800

    Initial Commit

---
+++ Shared/GoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodApp.swift
+//  Shared
+//
+//  Created by Emiaostein on 7/5/20.
+//
+
+import SwiftUI
+
+@main
+struct GoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
